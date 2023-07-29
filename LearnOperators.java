71f9c24cc184f0f008656b26385a67b757bbb2b0
madhumitha-ch <140213447+madhumitha-ch@users.noreply.github.com>
2023-07-30 00:49:42 +0530

    Add files via upload

---
+++ LearnOperators.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class LearnOperators {
+	
+	public static void main(String[] args) {
+	
+	System.out.println(6+5);
+	System.out.println(6-5);
+	System.out.println(16*6);
+	System.out.println(27%2);
+	System.out.println(26/2);
+
+	}
+}
